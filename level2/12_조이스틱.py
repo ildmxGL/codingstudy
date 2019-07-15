4dc3c788a8f6d46a34ceef5fdc4e4261be5afcac
ildmxGL <ildmx.GL@gmail.com>
2019-07-15 22:04:48 +0900

    week7

--- level2/12_조이스틱.py
+++ level2/12_조이스틱.py
@@ -1,61 +1,28 @@
-def find_mn(idx, name):
-    base = ['A'] * len(name)
-    name[idx] = 'A'
-    if name == base:
-        return 0
-    right, left = 0, 0
-    ridx, lidx = idx, idx
-    nw_right = [x for x in name]
-    nw_left = [x for x in name]
-    for _ in range(len(name)):
-        if nw_right[ridx] != 'A':
-            break
-        ridx = (ridx + 1) % len(name)
-        right += 1
-    right += find_mn(ridx, nw_right)
-    for _ in range(len(name)):
-        if nw_left[lidx] != 'A':
-            break
-        lidx = lidx - 1 if lidx - 1 < 0 else len(name) - 1
-        left += 1
-    left += find_mn(lidx, nw_left)
-
-    return left if left < right else right
-
 def solution(name):
     answer = 0
     tmp = 0
     print(name)
-    for x in name:
+    idxes = []
+    current_idx = 0
+    n = len(name)
+    for i, x in enumerate(name):
         tmp += ord(x) - ord('A') if ord(x) < ord('M') else ord('Z') + 1 - ord(x)
-    name = list(name)
-    idx = 0
-    answer += find_mn(idx, name)
-    """
-    while True:
-        ridx, lidx = 1, 1
-        tmp += ord(name[idx]) - ord('A') if ord(name[idx]) < ord('M') else ord('Z') + 1 - ord(name[idx])
-        name[idx] = 'A'
-        if name == base:
-            break
-        for i in range(1, len(name)):
-            if name[idx + i] == 'A':
-                ridx += 1
-            else:
-                break
-            if name[idx - i] == 'A':
-                lidx += 1
-            else:
-                break
-        if ridx > lidx:
-            answer += lidx
-            idx -= lidx
-        else:
-            answer += ridx
-            idx += ridx
-    """
+        if x != 'A':
+            idxes.append(i)
+    while idxes:
+        min_dist = float('inf')
+        min_idx = 0
+        for it in idxes:
+            min_dist2 = min(abs(it - current_idx), n - abs(it - current_idx))
+            if min_dist2 < min_dist:
+                min_dist = min_dist2
+                min_idx = it
+        answer += min_dist
+        idxes.remove(min_idx)
+        current_idx = min_idx
     print(answer)
     return answer + tmp
+
 
 print(solution('JAZ'))
 print(solution('JEROEN'))
--- level2/20_라면공장.py
+++ level2/20_라면공장.py
@@ -1,41 +1,19 @@
-"""
-# 효율성 테스트 #1, #3 Failure
-def solution(stock, dates, supplies, k):
-    answer = 0
-    today = 0
-    while stock < k - today:
-        idx = 0
-        for i in range(len(dates)):
-            if stock < dates[i] - today:
-                break
-            idx = i
-        stock -= dates[idx] - today
-        today = dates[idx]
-        nextday = dates[idx+1] if idx+1 < len(dates) else k
-        while stock < k - today and stock < nextday - today:
-            maxidx = supplies.index(max(supplies[:idx+1]), 0, idx+1)
-            dates.pop(maxidx)
-            stock += supplies.pop(maxidx)
-            idx -= 1
-            answer += 1
-    return answer
-"""
 import heapq
 def solution(stock, dates, supplies, k):
     answer = 0
     pq = []
     idx = 0
-    len_dates = len(dates)
-    for day in range(k):
-        if day == dates[idx]:
-            heapq.heappush(pq, -supplies[idx])
-            idx += 1 if idx < len_dates - 1 else 0
-        if not stock:
-            stock = -heapq.heappop(pq)
-            answer += 1
-        stock -= 1
+    while stock < k:
+        for i in range(idx, len(dates)):
+            if dates[i] <= stock:
+                heapq.heappush(pq, -supplies[i])
+                idx = i + 1
+            else:
+                break
+        stock -= heapq.heappop(pq)
+        answer += 1
     return answer
-
+    
 print(solution(4, [4, 10, 15], [20, 5, 10], 30))            #2
 print(solution(10, [5, 10], [1, 100], 110))                 #1
 print(solution(5, [1, 2, 3, 4, 5], [1, 1, 1, 1, 25], 30))   #1
