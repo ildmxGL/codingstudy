cca4de4fccf4987ac1393701abe9a7c26f1c2133
yeji201611160 <39490269+yeji201611160@users.noreply.github.com>
2019-07-27 01:29:56 +0900

    Update 04_네트워크_예지.py

--- level3/04_네트워크_예지.py
+++ level3/04_네트워크_예지.py
@@ -1,3 +1,25 @@
+'''
+def solution(n, computers):
+    lst =[]
+    for row in range(n):
+        S = set([col for col in range(n) if computers[row][col]])
+        for i in range(len(lst)):
+            if lst[i] & S:
+                lst[i] |= S
+                break
+        else:
+            lst.append(S)
+    print(lst)
+    return len(lst)
+
+print(solution(5, [[1,1,0,1,0],[1,1,0,0,0],[0,0,1,0,1],[1,0,0,1,1],[0,0,1,1,1]]))
+>[{0, 1, 2, 3, 4}, {2, 4}]
+2
+
+실제로는 하나의 네트워크라도
+서로 직접 연결되지 않은 노드들이 먼저 나오면 새로운 네트워크를 만들어버려서 안 됨.
+'''
+
 #그냥 네가 올린 DFS 그대로 함
 #computers에서 인덱스 안 가져오고 해보고 싶음
 
